cf8d852e587d213926c7be8bf75b2f8283d3ed92
IsaiahDahlberg <IsaiahD@magenic.com>
2019-06-21 15:55:11 -0500

    add css grid for Chart.js2 components

--- bill-tracker/src/chart/BarChart.js
+++ bill-tracker/src/chart/BarChart.js
@@ -5,17 +5,17 @@ import { throwStatement } from '@babel/types';
 function BarChart(props){  
     return (    
         <div  className="barChart">
-            <Bar                 
+            <Bar 
                 type= 'bar'
                 data={{
-                    labels: props.bills.map(bill => { return bill.Name }),
+                    labels: props.bills.map(bill => { return bill.Name }),                
                     datasets:[{
-                        labal:'Colors',
                         data: props.bills.map( bill => { return bill.Amount }),
                         backgroundColor: props.bills.map( bill => { return bill.Color }),                    
                     }]
                 }}
-                options={{ maintainAspectRatio: false, responsive: false}}
+                width={370}
+                options={{layout: { padding: { left: 5, right: 0, top: 15, bottom: 0 }}, maintainAspectRatio: false, responsive: false, legend:{display: false} }}
             />        
         </div>   
     )
